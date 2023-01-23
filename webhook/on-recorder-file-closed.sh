1774d0a95a01a2e5298dd810124c6f9ec32c405c
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-01-23 10:36:13 +0800

    shellcheck

--- webhook/on-recorder-file-closed.sh
+++ webhook/on-recorder-file-closed.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 record_file=$(echo "$1" | jq -r ".EventData.RelativePath")
-record_dir=$(dirname $record_file)
+record_dir=$(dirname "$record_file")
 echo "new record: $record_file"
 
 event_json_file="${record_file%.*}.json"
 echo "writing event to file $event_json_file"
-echo "$1" > $event_json_file
+echo "$1" > "$event_json_file"
 
 uid=$(baidupcs-go who | cut -d ',' -f 1 | grep -oE '[0-9]+')
 if [ "$uid" = 0 ]; then
