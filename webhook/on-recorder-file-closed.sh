5b94fd28aac5c0eefd26ed2e0d5704543b76319b
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-01-25 14:37:49 +0800

    add aliyunpan to webhook script

--- webhook/on-recorder-file-closed.sh
+++ webhook/on-recorder-file-closed.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -eu
+
 record_file=$(echo "$1" | jq -r ".EventData.RelativePath")
 record_dir=$(dirname "$record_file")
 echo "new record: $record_file"
@@ -8,11 +10,42 @@ event_json_file="${record_file%.*}.json"
 echo "writing event to file $event_json_file"
 echo "$1" | jq > "$event_json_file"
 
-uid=$(baidupcs-go who | cut -d ',' -f 1 | grep -oE '[0-9]+')
-if [ "$uid" = 0 ]; then
-    echo "not logged in to baidupcs. attempting login..."
-    baidupcs-go login -bduss="$BAIDUPCS_BDUSS" -stoken="$BAIDUPCS_STOKEN"
-fi
+upload_baidupcs() {
+    if [ ! "${BAIDUPCS_BDUSS+1}" ] || [ ! "${BAIDUPCS_STOKEN+1}" ]; then
+        exit
+    fi
 
-echo "uploading directory $record_dir"
-baidupcs-go upload "$record_dir" / && rm -rf "$record_dir"
+    loglist_length=$(baidupcs-go loglist | grep . | wc -l)
+    if [ "$loglist_length" -le 1 ]; then
+        echo "baidupcs not logged in. attempting login..."
+        baidupcs-go login -bduss="$BAIDUPCS_BDUSS" -stoken="$BAIDUPCS_STOKEN"
+    fi
+
+    echo "baidupcs uploading directory $record_dir"
+    baidupcs-go upload "$record_dir" /
+}
+
+upload_aliyunpan() {
+    if [ ! "${ALIYUNPAN_RTOKEN+1}" ]; then
+        exit
+    fi
+
+    loglist_length=$(aliyunpan loglist | grep . | wc -l)
+    if [ "$loglist_length" -le 1 ]; then
+        echo "aliyunpan not logged in. attempting login..."
+        aliyunpan login -RefreshToken="$ALIYUNPAN_RTOKEN"
+    fi
+
+    echo "aliyunpan uploading directory $record_dir"
+    aliyunpan upload "$record_dir" / 
+}
+
+upload_baidupcs &
+pid_baidupcs=$!
+
+upload_aliyunpan &
+pid_aliyunpan=$!
+
+wait $pid_baidupcs $pid_aliyunpan
+
+rm -rf "$record_dir"
