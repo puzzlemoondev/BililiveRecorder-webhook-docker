1354f4601d30d1408d36387a6cd3375fb235ae06
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-02-26 13:43:49 +0800

    add ignore warnings flag to DanmakuFactory command

--- webhook/command/danmaku_factory_command.py
+++ webhook/command/danmaku_factory_command.py
@@ -1,18 +1,12 @@
-from pathlib import Path
-
 from .command import Command
 
 
 class DanmakuFactoryCommand(Command):
-    """
-    DanmakuFactory expects user interaction when output exists.
-    We have to delete output file first to avoid this.
-    upstream issue: https://github.com/hihkm/DanmakuFactory/issues/43
-    """
-
     def __init__(self):
         super().__init__("DanmakuFactory")
 
+    def __call__(self, *args) -> str:
+        return super().__call__(*args, "--ignore-warnings")
+
     def convert(self, source: str, output: str, *args: str) -> str:
-        Path(output).unlink(missing_ok=True)
         return self("-i", source, "-o", output, *args)
