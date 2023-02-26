7fee7cf0fe008f22d827daeb14522b810ea26c0a
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-02-26 12:55:17 +0800

    fix DanmakuFactory retry

--- webhook/command/danmaku_factory_command.py
+++ webhook/command/danmaku_factory_command.py
@@ -1,9 +1,18 @@
+from pathlib import Path
+
 from .command import Command
 
 
 class DanmakuFactoryCommand(Command):
+    """
+    DanmakuFactory expects user interaction when output exists.
+    We have to delete output file first to avoid this.
+    upstream issue: https://github.com/hihkm/DanmakuFactory/issues/43
+    """
+
     def __init__(self):
         super().__init__("DanmakuFactory")
 
     def convert(self, source: str, output: str, *args: str) -> str:
-        return super().__call__("-i", source, "-o", output, *args)
+        Path(output).unlink(missing_ok=True)
+        return self("-i", source, "-o", output, *args)
