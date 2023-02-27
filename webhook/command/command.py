673cbff39910af9192cbfeb9cfb384be37a97bc7
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-02-27 13:24:37 +0800

    resolve command path

--- webhook/command/command.py
+++ webhook/command/command.py
@@ -1,9 +1,13 @@
+import shutil
 import subprocess
 
 
 class Command:
-    def __init__(self, executable: str):
-        self.executable = executable
+    def __init__(self, executable_name: str):
+        if executable := shutil.which(executable_name):
+            self.executable = executable
+        else:
+            raise FileNotFoundError(f"{executable_name} not found in path")
 
     def __call__(self, *args) -> str:
         return subprocess.check_output([self.executable, *args], encoding="utf8")
