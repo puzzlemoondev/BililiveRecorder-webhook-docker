d3082037b334382f8b38a9becb16a8ba439d9ff7
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-02-27 14:14:21 +0800

    symlink danmaku factory config

--- webhook/command/command.py
+++ webhook/command/command.py
@@ -1,16 +1,11 @@
 import shutil
 import subprocess
 from pathlib import Path
-from typing import Optional
 
 
 class Command:
-    def __init__(self, executable_name: str, cwd: Optional[Path] = None):
-        if executable := shutil.which(executable_name):
-            self.executable = executable
-        else:
-            raise FileNotFoundError(f"{executable_name} not found in path")
-        self.cwd = cwd
+    def __init__(self, executable_name: str):
+        self.executable = Path(shutil.which(executable_name))
 
     def __call__(self, *args) -> str:
-        return subprocess.check_output([self.executable, *args], cwd=self.cwd, encoding="utf8")
+        return subprocess.check_output([self.executable, *args], encoding="utf8")
--- webhook/command/danmaku_factory_command.py
+++ webhook/command/danmaku_factory_command.py
@@ -7,7 +7,13 @@ DANMAKU_FACTORY_CONFIG_DIR = Path("/etc/DanmakuFactory").resolve(strict=True)
 
 class DanmakuFactoryCommand(Command):
     def __init__(self):
-        super().__init__("DanmakuFactory", cwd=DANMAKU_FACTORY_CONFIG_DIR)
+        super().__init__("DanmakuFactory")
+
+        config = DANMAKU_FACTORY_CONFIG_DIR.joinpath("DanmakuFactoryConfig.json")
+        if config.exists():
+            system_config = self.executable.with_name(config.name)
+            system_config.unlink(missing_ok=True)
+            system_config.symlink_to(config)
 
     def __call__(self, *args) -> str:
         return super().__call__("-c", *args, "--ignore-warnings")
