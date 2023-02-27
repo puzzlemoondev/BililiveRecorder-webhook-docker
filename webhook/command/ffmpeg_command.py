b768aae873de634733c3ecd717f94921aa42c0d8
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-02-27 21:23:14 +0800

    remove ffmpeg file extension check

--- webhook/command/ffmpeg_command.py
+++ webhook/command/ffmpeg_command.py
@@ -9,9 +9,6 @@ class FFMPEGCommand(Command):
         super().__init__("ffmpeg")
 
     def add_subtitles(self, source: str, subtitles: str, output: str) -> str:
-        if Path(output).suffix != ".mp4":
-            raise ValueError(f"invalid output extension: {output}")
-
         with TemporaryDirectory() as tmpdir:
             ass = Path(tmpdir).joinpath("subtitles.ass")
             ass.symlink_to(subtitles)
