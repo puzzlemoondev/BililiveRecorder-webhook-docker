834a9bf08afa519ca34cdebc8b6882058737f083
PuzzleMoon <32855965+puzzlemoondev@users.noreply.github.com>
2023-03-19 15:54:39 +0800

    baidupcs max upload parallel config

--- webhook/config/config.py
+++ webhook/config/config.py
@@ -7,6 +7,7 @@ from typing import Optional
 class Config:
     baidupcs_bduss: Optional[str] = field(init=False)
     baidupcs_stoken: Optional[str] = field(init=False)
+    baidupcs_max_upload_parallel: Optional[str] = field(init=False)
     aliyunpan_rtoken: Optional[str] = field(init=False)
     burn_danmaku: bool = field(init=False)
     bilibili_upload_burned: bool = field(init=False)
@@ -15,6 +16,7 @@ class Config:
     def __post_init__(self):
         self.baidupcs_bduss = os.environ.get("BAIDUPCS_BDUSS") or None
         self.baidupcs_stoken = os.environ.get("BAIDUPCS_STOKEN") or None
+        self.baidupcs_max_upload_parallel = os.environ.get("BAIDUPCS_MAX_UPLOAD_PARALLEL")
         self.aliyunpan_rtoken = os.environ.get("ALIYUNPAN_RTOKEN") or None
         self.burn_danmaku = os.environ.get("BURN_DANMAKU", "0") != "0"
         self.bilibili_upload_burned = os.environ.get("BILIBILI_UPLOAD_BURNED", "0") != "0"
